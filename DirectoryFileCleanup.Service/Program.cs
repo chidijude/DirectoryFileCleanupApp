31d80ee4b12d3180472776baedd4b3a56d4e95c4
Chidi Jude <cjude@appzonegroup.com>
2022-09-02 13:32:31 +0100

    Add project files.

---
+++ DirectoryFileCleanup.Service/Program.cs
@@ -0,0 +1,10 @@
+using DirectoryFileCleanup.Service;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
